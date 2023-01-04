3ff71be0e6770f58774fc99ab805eed6168a4e59
Fabio <fabioramosnic@gmail.com>
2023-01-04 07:58:23 -0300

    Creacion de nuevos modulos (expreriencias, proyectos y capasidades)

---
+++ src/app/Components/capacidades/capacidades.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-capacidades',
+  templateUrl: './capacidades.component.html',
+  styleUrls: ['./capacidades.component.css']
+})
+export class CapacidadesComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,21 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './Components/nav/nav.component';
 import { FooterComponent } from './Components/footer/footer.component';
 import { InicioComponent } from './Components/inicio/inicio.component';
+import { ExperienciasComponent } from './Components/experiencias/experiencias.component';
+import { CapacidadesComponent } from './Components/capacidades/capacidades.component';
+import { ProyectosComponent } from './Components/proyectos/proyectos.component';
+import { LoginComponent } from './Components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     FooterComponent,
-    InicioComponent
+    InicioComponent,
+    ExperienciasComponent,
+    CapacidadesComponent,
+    ProyectosComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
